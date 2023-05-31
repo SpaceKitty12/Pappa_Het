800c10a0c819ddf938f50378d031cb724f27bc66
SpaceKitty12 <shuffo0080@gmail.com>
2023-05-31 13:37:28 -0500

    tlhngan Hol: qar 6 moj

--- brain_two/todos/admin.py
+++ brain_two/todos/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
-from .models import TodoList
+from .models import TodoList, TodoItem
 
 
 class TodoListAdmin(admin.ModelAdmin):
     list_display = ("id", "name")
 
 
+class TodoItemAdmin(admin.ModelAdmin):
+    list_display = ("task", "due_date", "is_completed")
+
+
 admin.site.register(TodoList, TodoListAdmin)
+admin.site.register(TodoItem, TodoItemAdmin)
 
 
 # test, do do list wasn't showing up in admin page  - #yeah this is what was missing!
