ad5a4a0349ff73d81598c3c19ccd993dfa13a480
SpaceKitty12 <shuffo0080@gmail.com>
2023-05-31 12:43:50 -0500

    Funcionalidad 4 comletada

--- brain_two/todos/admin.py
+++ brain_two/todos/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import TodoList
 
-# Register your models here.
+
+class TodoListAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+
+
+admin.site.register(
+    TodoList, TodoListAdmin
+)  # test, do do list wasn't showing up in admin page  - #yeah this is what was missing!
+
+# REMEMBER THIS SCOTT ^^^^^^^^^^^^^^^^^^^  ya dumb ass
