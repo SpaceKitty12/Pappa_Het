27fab99bf24330b63c615cf96b8fa41e0e787bca
SpaceKitty12 <shuffo0080@gmail.com>
2023-05-31 20:19:23 -0500

    mine does all the stuff.  I did it a different way, named stuff different stuff etc..  it all works.  I can add CSS, better HTML, cleaner buttons... when it isnt' costing 20k, otherwise all good!  enough of this, cya tomorrow

--- brain_two/todos/views.py
+++ brain_two/todos/views.py
@@ -18,7 +18,7 @@ def todo_list_detail(request, id):
         {"todo_list": todo_list, "tasks": tasks},
     )
 
-    # jen may 31    MU*ST MATCH NAMES FDLKJKRKT":P@#QR  #####SCREEN SHOTS@@######### come crawling fasterrrrrrrrr, #your lyric here.
+    # jen may 31    MU*ST MATCH NAMES FDLKJKRKT":P@#QR  #####SCREEN SHOTS@@######### come crawling fasterrrrrrrrr, #your lyric here.  YOUR LIFE BURNS FASTERRRRRR. your lyric hereman
 
     # IMPORTANT#
 
